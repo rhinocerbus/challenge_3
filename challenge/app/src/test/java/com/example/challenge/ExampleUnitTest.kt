4a29cc1192a8de3c6427408977957ae9d9ce2d0b
Matt Rubright <m.rubright.dev@gmail.com>
2020-09-25 23:09:01 -0400

    project files, challenge sample resources

---
+++ challenge/app/src/test/java/com/example/challenge/ExampleUnitTest.kt
@@ -0,0 +1,28 @@
+package com.example.challenge
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+
+        /*
+
+
+        val viewModel =  ViewModelProvider(this).get(ArmorListViewModel::class.java)
+        viewModel.armorList.observe(this, Observer {
+            assert(it.isNotEmpty())
+        })
+
+        viewModel.loadArmorData()
+        */
+    }
+}
